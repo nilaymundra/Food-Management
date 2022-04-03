41f0e76400e4ad75bdc26f5e412cd18e504bdf4e
Nilay <nilaysmundra@gmail.com>
2022-04-03 11:06:25 +0530

    Add Users Model

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ const express = require('express');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 const ejs = require('ejs');
+const bodyParser = require('body-parser');
 
 const app = express();
 
 dotenv.config();
 app.use(express.static("public"));
 app.set("view engine", "ejs");
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 const url = process.env.MONGO_URI;
 const PORT = process.env.PORT || 3000
